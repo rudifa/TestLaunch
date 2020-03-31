89de3c2b4a7d676923c688b4297b07a3672e8578
Rudi Farkas <rudi.farkas@gmail.com>
2020-03-31 17:23:34 +0200

    Initial Commit

---
+++ TestLaunch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestLaunch
+//
+//  Created by Rudolf Farkas on 31.03.20.
+//  Copyright © 2020 Rudolf Farkas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
